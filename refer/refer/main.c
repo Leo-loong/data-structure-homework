5469df3ae0ad3031663e9bf00695912298fd2ae6
leo-loong <24376280@buaa.edu.cn>
2026-04-02 17:06:28 +0800

    3rd-homework-2nd

---
+++ refer/refer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  refer
+//
+//  Created by 匡萍 on 2026/3/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
